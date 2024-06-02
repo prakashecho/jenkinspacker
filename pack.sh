fa18688da605c33707db60c642b47bd20933ce32
prakashecho <50212384+prakashecho@users.noreply.github.com>
2024-06-02 20:24:24 +0530

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# Access the AMI ID passed as an argument
-AMI_ID=$AMI_ID
-# Revoke launch permissions for a specific AWS account
+AMI_ID=$(jq -r '.builds[-1].artifact_id' manifest.json | cut -d ':' -f 2)
+
+aws ec2 copy-image \
+  --source-image-id $AMI_ID \
+  --source-region us-east-1 \
+  --name "Jenkins-AMI-Copy" \
+  --region us-west-2
+
 aws ec2 modify-image-attribute \
-    --image-id "$AMI_ID" \
-    --launch-permission "Remove=[{UserId=280435798514}]"
+  --image-id $AMI_ID \
+  --launch-permission "Add=[{UserId=280435798514}]" \
+  --region us-east-1
